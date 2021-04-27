2cb3bd240bf2a63c551fc2ee150d9720a994e070
Ramadhana Bhanuharya Wishnumurti <wishnumurti312@gmail.com~>
2021-04-27 21:47:47 +0700

    coba2 web

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 
@@ -8,3 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    if(userText == "halo"):
+        # return str(english_bot.get_response(userText))
+        return str('Haro, Warudo!')
