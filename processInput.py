1aa8031a3ae18452e0b21a5326d730190e29d361
Rafidika Samekto <13519207@std.stei.itb.ac.id>
2021-04-28 16:33:16 +0700

    fix process input dikit

--- processInput.py
+++ processInput.py
@@ -14,6 +14,46 @@ def removeNewLine(string):
             newString = newString + char
     return newString
 
+def checkIfTanggalExist(command):
+    matchObject = None
+    allowedDateFormat = ['\d\d-\d\d-\d\d\d\d','\d\d\/\d\d\/\d\d\d\d']
+    for format in allowedDateFormat: # Pasti date yang ada sesuai sama salah satu format atau ga sama sekali
+        if (re.search(format,command)):
+            matchObject = re.search(format,command)
+    if (matchObject): # Ada tanggal dalam command
+        return True
+    else:   # Tidak ada tanggal dalam command
+        return False
+
+def checkIfKodeExist(command):
+    matchObject = None
+    allowedKodeFormat = '[A-Z][A-Z]\d\d\d\d'
+    if (re.search(allowedKodeFormat,command)):
+        matchObject = re.search(allowedKodeFormat,command)
+    if (matchObject): # Ada kode matkul
+        return True
+    else: # Tidak ada kode matkul
+        return False
+
+def checkIfTopikExist(command):
+    matchObject = None
+    if (re.search("\"(\w|\s)*\"|\'(\w|\s)*\'",command)):
+        matchObject = re.search("\"(\w|\s)*\"|\'(\w|\s)*\'",command)
+    if (matchObject):
+        return True
+    else:
+        return False
+
+def checkIfIDExist(command):
+    matchObject = None
+    allowedIDFormat = '\s[0-9]*\s'
+    if (re.search(allowedIDFormat,command)):
+        matchObject = re.search(allowedIDFormat,command)
+    if (matchObject):
+        return True
+    else:
+        return False
+
 # Determine what kind of process (add, update, or mark as done)
 def processInput(command):
     # Command List
@@ -48,15 +88,18 @@ def processInput(command):
     
     # Process command
     for update in listUpdateCommand:
-        if (kmpMatch(command,update) != -1):
+        if ((kmpMatch(command,update) != -1)  and (checkIfTanggalExist(command)) and (checkIfIDExist(command))):
+            # Mengandung salah satu keyword untuk update, tanggal baru, dan ID Task
             return handleUpdateTask(command)
     
     for done in listDoneCommand:
-        if (kmpMatch(command,done) != -1):
+        if ((kmpMatch(command,done) != -1) and (checkIfIDExist(command))):
+            # Mengandung salah satu keyword untuk done dan ID Task
             return handleMarkDoneTask(command)
     
-    for task in listTask:
-        if (kmpMatch(command,task) != -1):
+    for task in listTask: # Kalo mau pake command add task harus ada:
+        if ((kmpMatch(command,task) != -1) and (checkIfTanggalExist(command)) and (checkIfKodeExist(command)) and (checkIfTopikExist(command))):
+            # Mengandung salah satu keyword task (PR,Kuis,Praktikun,UTS,UAS,Tucil,Tubes), tanggal, kode matkul, dan topik task dalam command
             return handleNewTask(command,task)
 
     if(kmpMatch(command.lower(), "apa saja") != -1 and kmpMatch(command.lower(), "deadline")!=-1):
